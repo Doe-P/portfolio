6ada6d10a76b607276da485136bac2d4afc67d0c
Doe-P <phoukhando@gmail.com>
2023-12-15 14:48:54 +0700

    add web title logo and resume

--- src/contents/header.js
+++ src/contents/header.js
@@ -1,9 +1,10 @@
-import {reactive, ref} from "vue";
+import {reactive} from "vue";
+import Resume from "../../static/resume-1.pdf"
 
 export const data = reactive({
     name:'Phoukhan Xayavongsone',
     title: 'Front-End & Mobile App Developer',
     description: 'From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.',
-    link: 'https://www.facebook.com/?_rdc=2&_rdr',
+    link: Resume,
     buttonText: 'View Resume',
 });
